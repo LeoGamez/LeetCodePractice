0430818d0f07e5ef4bc46a222ffeb30b0f8009c7
Leonardo Gamez <leonardo.gamez@h1ve.com.mx>
2023-08-26 14:23:48 -0600

    TwoSum, initial

---
+++ src/lgLeetCodePractice.Tests/TwoSumSolverTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using lgLeetCodePractice;
+using FluentAssertions;
+
+namespace lgLeetCodePractice.Tests
+{
+    public class TwoSumSolverTests
+    {
+        [Theory]
+        [InlineData(new int[] { 15, 7, 2, 11 },9, new int[] { 1,2 })]
+        [InlineData(new int[] { 3, 2, 4 },6, new int[] { 1,2 })]
+        [InlineData(new int[] { 3,1, 3 },6, new int[] { 0,2 })]
+        [InlineData(new int[] { 3,-1, 2 },1, new int[] { 1,2 })]
+        public void TwoSumResults(int[] nums, int target, int[] expected)
+        {
+            // Arrange
+            var twoSumSolver = new TwoSumSolver();
+
+            // Act
+            var result = twoSumSolver.TwoSum(
+                nums,
+                target);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/lgLeetCodePractice/lgLeetCodePractice/TwoSum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace lgLeetCodePractice
+{
+    public class TwoSumSolver
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            int delta = target;
+            int[]sorted=nums.ToArray();
+            Array.Sort(sorted);
+            while(left < right)
+            {
+                int current = sorted[left] + sorted[right]-target;
+
+                if (current > 0)
+                {
+                    right--;
+                }
+                else if (current < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    int first = Array.IndexOf(nums, sorted[left]);
+                    int second= Array.IndexOf(nums, sorted[right]);
+                    while(first==second)
+                    {
+                        second = Array.IndexOf(nums, sorted[right],first+1);
+                    }
+                    return new int[] { first, second };
+                }
+            }
+
+            return new List<int>().ToArray();
+        }
+    }
+}
