8e278772372443a5babfbfae787620af0b75701d
Leonardo Gamez <leonardo.gamez@h1ve.com.mx>
2023-08-28 21:49:17 -0600

    added divide two integers

---
+++ src/lgLeetCodePractice.Tests/DivideTwoIntegersSolverTests.cs
@@ -0,0 +1,58 @@
+﻿using Moq;
+using System;
+using Xunit;
+using lgLeetCodePractice;
+using FluentAssertions;
+
+namespace lgLeetCodePractice.Tests
+{
+    public class DivideTwoIntegersSolverTests
+    {
+        private MockRepository mockRepository;
+
+
+
+        public DivideTwoIntegersSolverTests()
+        {
+
+
+        }
+
+        private DivideTwoIntegersSolver CreateDivideTwoIntegersSolver()
+        {
+            return new DivideTwoIntegersSolver();
+        }
+
+        [Theory]
+        [InlineData(0,0,0)]
+        [InlineData(0,1,0)]
+        [InlineData(1,1,1)]
+        [InlineData(1,2,0)]
+        [InlineData(3,2,1)]
+        [InlineData(4,2,2)]
+        [InlineData(6,2,3)]
+        [InlineData(100,5,20)]
+        [InlineData(101,5,20)]
+        [InlineData(105,5,21)]
+        [InlineData(104,5,20)]
+        [InlineData(2000,110,18)]
+        [InlineData(-1,1,-1)]
+        [InlineData(2,-1,-2)]
+        [InlineData(-25,-5,5)]
+        [InlineData(int.MinValue, -1, int.MaxValue)]
+        [InlineData(int.MinValue, 1, int.MinValue)]
+        public void Divide(int dividend, int divisor, int? expected)
+        {
+            // Arrange
+            var divideTwoIntegersSolver = this.CreateDivideTwoIntegersSolver();
+
+            // Act
+            var result = divideTwoIntegersSolver.Divide(
+                dividend,
+                divisor);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ src/lgLeetCodePractice/lgLeetCodePractice/DivideTwoIntegersSolver.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lgLeetCodePractice
+{
+    public class DivideTwoIntegersSolver
+    {
+        public int Divide(int dividend, int divisor)
+        {
+            if(divisor == 0)
+            {
+                return 0;
+            }
+            else if (dividend == 0)
+            {
+                return 0;
+            }
+
+            if (divisor == 1)
+            {
+                return dividend;
+            }
+
+            if (divisor == -1)
+            {
+                if (dividend == Int32.MinValue)
+                {
+                    return Int32.MaxValue;
+                }
+                else
+                {
+                    return -dividend;
+                }
+            }
+
+            int i = 0;
+            if (dividend >= 0 && divisor > 0)
+            {
+                while (dividend >= divisor)
+                {
+                    dividend -= divisor;
+                    i++;
+                }
+            }
+            else if (dividend < 0 && divisor < 0)
+            {
+                while (dividend <= divisor)
+                {
+                    dividend -= divisor;
+                    i++;
+                }
+            }
+            else if (dividend >= 0 && divisor < 0)
+            {
+                while (-dividend <= divisor)
+                {
+                    dividend += divisor;
+                    i--;
+                }
+            }
+            else if (dividend < 0 && divisor > 0)
+            {
+                while (dividend <= -divisor)
+                {
+                    dividend += divisor;
+                    i--;
+                }
+            }
+            return i;
+        }
+    }
+}
