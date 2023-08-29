03706b823ac48d1e2feb0a35771f091f08934e37
Leonardo Gamez <leonardo.gamez@h1ve.com.mx>
2023-08-28 22:23:25 -0600

    Added SearchInsert

---
+++ src/lgLeetCodePractice.Tests/SearchInsertSolverTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using System;
+using Xunit;
+using lgLeetCodePractice;
+using FluentAssertions;
+
+namespace lgLeetCodePractice.Tests
+{
+    public class SearchInsertSolverTests
+    {
+        private MockRepository mockRepository;
+
+        private SearchInsertSolver CreateSearchInsertSolver()
+        {
+            return new SearchInsertSolver();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 5, 2)]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 2, 1)]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 6, 3)]
+        [InlineData(new int[] { 1, 3, 5, 7 }, 7, 3)]
+        [InlineData(new int[] { 1, 3, 5, 6 }, 7, 4)]
+        [InlineData(new int[] {2, 3, 5, 6 }, 1, 0)]
+        public void SearchInsert(int[] nums, int target, int expected)
+        {
+            // Arrange
+            var searchInsertSolver = this.CreateSearchInsertSolver();
+
+            // Act
+            var result = searchInsertSolver.SearchInsert(nums, target);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ src/lgLeetCodePractice/lgLeetCodePractice/SearchInsertSolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lgLeetCodePractice
+{
+    public class SearchInsertSolver
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int ix = 0;
+            while (nums[ix] < target && ix<nums.Length-1)
+            {
+                 ix++;
+            }
+
+            if (target - nums[ix] > 0)
+            {
+                ix++;
+            }
+
+            return ix;
+        }
+    }
+}
